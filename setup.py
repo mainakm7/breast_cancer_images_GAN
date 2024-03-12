3972f716ed3317ef45e500d3aa7797fa3a6bdf8f
mainakm7 <mainak.mustafi@gmail.com>
2024-03-12 14:55:07 -0400

    Made a new repository for Breast Cancer Hispathology image generation using GANs Made the file training_GAN_1.ipynb as initial testing Added kaggle dataset to data/raw

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Training a GAN model to generate breast cancer hispathology images',
+    author='Mainak Mustafi',
+    license='MIT',
+)
